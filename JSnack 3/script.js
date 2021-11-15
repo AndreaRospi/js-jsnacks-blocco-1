d6dbb7765b218845d4f14272d385a9850c6d761d
Andrea <andr3z45@gmail.com>
2021-11-15 16:05:53 +0100

    js snack 3 completato

--- JSnack 3/script.js
+++ JSnack 3/script.js
@@ -1,9 +1,8 @@
 let numero = 2;
 
 while (numero < 1000) {
+    console.log(' ' + numero + ' ');
+
     numero = numero * 2;
-    if (numero < 1000) {
-        console.log(' ' + numero + ' ');
-    }
 
 }
